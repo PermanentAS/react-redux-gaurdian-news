037d10a78f1f4203218a8d529359c2617e1f298d
Anton Shlykov <permanent.as@gmail.com>
2019-08-29 15:51:52 +0300

    Updates

---
+++ precache-manifest.5915e1cf2a742e712e8bad7645b0e97f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af1b50081c3a6ac4ab1520c553ce058",
+    "url": "/react-redux-gaurdian-news/index.html"
+  },
+  {
+    "revision": "4d4f7dca2fcb698f981d",
+    "url": "/react-redux-gaurdian-news/static/css/main.3d635d03.chunk.css"
+  },
+  {
+    "revision": "e72f6518d6363cf79313",
+    "url": "/react-redux-gaurdian-news/static/js/2.6815a1b9.chunk.js"
+  },
+  {
+    "revision": "4d4f7dca2fcb698f981d",
+    "url": "/react-redux-gaurdian-news/static/js/main.254e0448.chunk.js"
+  },
+  {
+    "revision": "f5db449007bf583368d2",
+    "url": "/react-redux-gaurdian-news/static/js/runtime~main.2962aa9e.js"
+  }
+]);
